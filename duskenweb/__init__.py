9f4298492ce531d7a8017040a78d0f12ea351b2f
Nikolai Kristiansen <nikolaik@gmail.com>
2013-12-28 00:21:19 +0100

    Configurable dusken base_url

--- duskenweb/__init__.py
+++ duskenweb/__init__.py
@@ -1,9 +1,10 @@
+# TODO this is not the right way to do this, move this to another place
 import pydusken
 from django.conf import settings
 
 # Init DuskenAPI
-system_api_conn = pydusken.DuskenApi(settings.DUSKEN_CLIENT_ID, settings.DUSKEN_CLIENT_SECRET)
+system_api_conn = pydusken.DuskenApi(settings.DUSKEN_CLIENT_ID, settings.DUSKEN_CLIENT_SECRET, settings.DUSKEN_BASE_URL)
 if not system_api_conn.is_authenticated():
     system_api_conn.authenticate(settings.DUSKEN_USERNAME, settings.DUSKEN_PASSWORD)
 
-user_api_conn = pydusken.DuskenApi(settings.DUSKEN_CLIENT_ID, settings.DUSKEN_CLIENT_SECRET)
+user_api_conn = pydusken.DuskenApi(settings.DUSKEN_CLIENT_ID, settings.DUSKEN_CLIENT_SECRET, settings.DUSKEN_BASE_URL)
--- duskenweb/settings-sample.py
+++ duskenweb/settings-sample.py
@@ -106,6 +106,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = "/profile/"
 LOGIN_URL = "/login/"
 
+DUSKEN_BASE_URL= 'https://duskendev.neuf.no'
 DUSKEN_CLIENT_ID = ''
 DUSKEN_CLIENT_SECRET = ''
 # Dusken API system user
